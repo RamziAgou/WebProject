05f3faf4a37124f5dee6e13b5626315a8e10ad69
alexdissy <alexandre.avakian@edu.ece.fr>
2019-12-13 20:30:25 +0100

    Creation of front-end including Login, register and home

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,24 @@ const port: string = process.env.PORT || '8080'
 
 app.get('/', (req: any, res: any) => {
   res.render("home.ejs");
+  
+})
+
+app.get('/login', (req: any, res: any) => {
+  res.render("login.ejs");
+  // res.send('login')
+})
+
+app.get('/register', (req: any, res: any) => {
+  res.render("register.ejs");
+  //res.send('register')
+})
+
+app.post('/register', (req: any, res: any) => {
+
+  console.log(req.body)
+  res.send("hello");
+  //res.send('register')
 })
 
 app.listen(port, (err: Error) => {
